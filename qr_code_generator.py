4dd4107884089ff4f00e40574c35e196a42c5843
Tina Iuso <tinaiuso@gmail.com>
2021-07-28 17:24:53 +0100

    qr code generated

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,16 +1,17 @@
-import qrcode 
+import qrcode
  
 #Link to the CV 
 input_data = "https://drive.google.com/file/d/1zkuv1_Tpuc1z1CGaGg8f6VS6hCrAO-ER/view?usp=sharing"
 
 #Instance of qrcode
 qr = qrcode.QRCode(
-        version = 2
-        box_size = 10
-        border = 5)
+    version=2,
+    box_size=10,
+    border=5,
+)
 
 qr.add_data(input_data)
 qr.make(fit=True)
 
-img = qr.make_image(fill = 'blue', back_color='white')
+img = qr.make_image(fill_color='midnightblue', back_color='white')
 img.save('qrcodeCV.png')
